f5efdf08511d53e44fa1d4dfe86db8e93e68884a
Abhi <abhi.sharma795@gmail.com>
2021-02-14 12:38:35 +0530

    Updates

---
+++ precache-manifest.2e2e500931703a57b7108967fe4851cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128f94d3877b8d1783cd9befea47e5e3",
+    "url": "/thisisabhisharma/old-friend/index.html"
+  },
+  {
+    "revision": "28f457ed011d5f58af46",
+    "url": "/thisisabhisharma/old-friend/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "e2177d6283247821d6d3",
+    "url": "/thisisabhisharma/old-friend/static/css/main.0f1a7a3e.chunk.css"
+  },
+  {
+    "revision": "28f457ed011d5f58af46",
+    "url": "/thisisabhisharma/old-friend/static/js/2.ae42f685.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thisisabhisharma/old-friend/static/js/2.ae42f685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2177d6283247821d6d3",
+    "url": "/thisisabhisharma/old-friend/static/js/main.af441d1e.chunk.js"
+  },
+  {
+    "revision": "27b12b2704613bb7b2ec",
+    "url": "/thisisabhisharma/old-friend/static/js/runtime-main.0485e314.js"
+  },
+  {
+    "revision": "1914581596f1f65815beac8b8ffa1ca1",
+    "url": "/thisisabhisharma/old-friend/static/media/school_image.19145815.jpg"
+  }
+]);
